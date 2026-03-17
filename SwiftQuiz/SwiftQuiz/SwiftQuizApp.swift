3787302a432cf0afbb11febda733f0e571c8a898
Даша Суворова <dasha@192.168.100.4>
2026-03-17 22:29:43 +0300

    first commit

---
+++ SwiftQuiz/SwiftQuiz/SwiftQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftQuizApp.swift
+//  SwiftQuiz
+//
+//  Created by Даша Суворова on 17.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
